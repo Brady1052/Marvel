85e6818a4a67d1f84501c05f5bc8ac2e35bf95b0
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-12 22:14:12 -0400

    added comments. App now displays name and description of character searched

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 //HTML Elements
-const characterSearchBox = document.getElementById('characterSearch');
-const characterSearchBtn = document.getElementById('characterSearchBtn');
-var characterName = document.getElementById('charName');
-//Params for query
-const hash = '652341e1c91874a97bbf4aa8091c2ab3'
-const publicKey = '23e4d1775db55e165690e462a3630e80'
 
+//The actual input box where the user types the name of the character they want to search
+const characterSearchBox = document.getElementById('characterSearch');
+
+//The buttons that users click when they go to search for the character
+const characterSearchBtn = document.getElementById('characterSearchBtn');
+
+//H1 Element that displays name of character that was searched
+const characterName = document.getElementById('charName');
+
+//H1 element that displays "Description" upon character search. This element is located directly above the character description
+const descriptionHeader = document.getElementById('aboutCharh1');
+
+//p element that displays description of char searched
+const aboutCharacter = document.getElementById('aboutChar');
+
+//Params for query
+const hash = '652341e1c91874a97bbf4aa8091c2ab3';
+const publicKey = '23e4d1775db55e165690e462a3630e80';
+
+
+//Fetch Api Function
 function fetchData(){
     fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publicKey}&hash=${hash}&name=${characterSearchBox.value}`)
 .then(res => res.json())
@@ -13,7 +28,9 @@ function fetchData(){
  console.log(info)   
 const charName = info.data.results[0].name;
 console.log(charName)
-characterName.textContent = charName
+characterName.innerHTML = charName
+
+aboutCharacter.innerHTML = info.data.results[0].description;
 
 
 })
